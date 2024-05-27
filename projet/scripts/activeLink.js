c31b2b6072653219901f17e759d451a2b3c46e85
Issammo12 <102838288+Issammo12@users.noreply.github.com>
2024-05-27 20:45:01 +0100

    Delete projet directory

--- projet/scripts/activeLink.js
+++
@@ -1,7 +0,0 @@
-const pathName=window.location.pathname;
-const links=document.querySelectorAll('a');
-links.forEach(link=>{
-    if(link.href.includes(`${pathName}`)){
-        link.classList.add('active')
-    }
-})
